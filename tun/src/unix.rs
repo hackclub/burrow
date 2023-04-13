02efa85a19b2a33052e9709d0e40023c5c1a2b91
dav <usr21p1@gmail.com>
2023-04-12 19:30:03 -0700

    Fix types and fix build + clippy lints for linux
    This commit changes `copy_if_name` to take a c_char.

--- tun/src/unix.rs
+++ tun/src/unix.rs
@@ -1,6 +1,6 @@
-use std::ffi::CStr;
+use std::ffi::{c_char, CStr};
 
-pub fn copy_if_name(buf: [u8; libc::IFNAMSIZ]) -> String {
+pub fn copy_if_name(buf: [c_char; libc::IFNAMSIZ]) -> String {
     // TODO: Switch to `CStr::from_bytes_until_nul` when stabilized
     unsafe {
         CStr::from_ptr(buf.as_ptr() as *const _)
