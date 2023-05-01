65598d05665f9783afbd094924e4a382a16bf62c
Malted <malted@malted.dev>
2023-05-01 10:00:23 +0100

    🎨 Combine interface and gateway IP
    Uses the same IP instance for the gateway as the interface

--- burrow/src/main.rs
+++ burrow/src/main.rs
@@ -7,8 +7,7 @@ async fn try_main() -> Result<()> {
     let interface = TunInterface::new()?;
     let interface_addr = Ipv4Addr::new(10, 1, 0, 1);
     let dest = Ipv4Addr::new(0, 0, 0, 0);
-    let gateway = Ipv4Addr::new(10, 1, 0, 1);
-    routing_table::reroute(interface, interface_addr, dest, gateway).await;
+    routing_table::reroute(interface, interface_addr, dest, interface_addr).await;
 
     Ok(())
 }
