9b2a2b859d64c473b932d05b1f12a6d088f1199f
Malted <malted@malted.dev>
2023-05-01 16:59:03 +0100

    🛂 Check for required permissions
    On Linux, checks for the `CAP_NET_ADMIN` capability.
    On macOS, checks for root.

--- burrow/src/main.rs
+++ burrow/src/main.rs
@@ -2,6 +2,8 @@ use tokio::io::Result;
 use tun::TunInterface;
 
 async fn try_main() -> Result<()> {
+    burrow::ensure_root!();
+
     let iface = TunInterface::new()?;
     println!("{:?}", iface.name());
 
