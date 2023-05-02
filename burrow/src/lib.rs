16157318e22aa80cc5c8d634b5813324ece8d5c6
Malted <malted@malted.dev>
2023-05-02 15:28:26 +0100

    ⚗️ Remove caps dependency
    Libc was failing to compile on macos & windows

--- burrow/src/lib.rs
+++ burrow/src/lib.rs
@@ -1,33 +1,33 @@
-#[macro_export]
-macro_rules! ensure_root {
-    () => {
-        // Check for root user on macOS
-        #[cfg(target_os = "macos")]
-        {
-            use nix::unistd::Uid;
+// // Check capabilities on Linux
+// #[cfg(target_os = "linux")]
+// fn ensure_root() {
+//     use caps::{has_cap, CapSet, Capability};
 
-            let current_uid = Uid::current();
-            if !current_uid.is_root() {
-                eprintln!("This program must be run as root!");
-                std::process::exit(1);
-            }
-        }
+//     let cap_net_admin = Capability::CAP_NET_ADMIN;
+//     if let Ok(has_cap) = has_cap(None, CapSet::Effective, cap_net_admin) {
+//         if !has_cap {
+//             eprintln!("This program must be run with CAP_NET_ADMIN!");
+//             std::process::exit(1);
+//         }
+//     } else {
+//         eprintln!("Failed to check capabilities");
+//         std::process::exit(1);
+//     }
+// }
 
-        // Check capabilities on Linux
-        #[cfg(target_os = "linux")]
-        {
-            use caps::{has_cap, CapSet, Capability};
+// Check for root user on macOS
+#[cfg(target_family = "unix")]
+pub fn ensure_root() {
+    use nix::unistd::Uid;
 
-            let cap_net_admin = Capability::CAP_NET_ADMIN;
-            if let Ok(has_cap) = has_cap(None, CapSet::Effective, cap_net_admin) {
-                if !has_cap {
-                    eprintln!("This program must be run with CAP_NET_ADMIN!");
-                    std::process::exit(1);
-                }
-            } else {
-                eprintln!("Failed to check capabilities");
-                std::process::exit(1);
-            }
-        }
-    };
+    let current_uid = Uid::current();
+    if !current_uid.is_root() {
+        eprintln!("This program must be run as root!");
+        std::process::exit(1);
+    }
+}
+
+#[cfg(target_family = "windows")]
+pub fn ensure_root() {
+    todo!()
 }
--- burrow/src/main.rs
+++ burrow/src/main.rs
@@ -2,7 +2,7 @@ use tokio::io::Result;
 use tun::TunInterface;
 
 async fn try_main() -> Result<()> {
-    burrow::ensure_root!();
+    burrow::ensure_root();
 
     let iface = TunInterface::new()?;
     println!("{:?}", iface.name());
