b00d520d47596d01d6e33780f457ebce29c69e33
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-06-01 11:48:33 -0600

    Finished onboarding screen
    Final changes to the onboarding screen

--- Apple/App/BurrowApp.swift
+++ Apple/App/BurrowApp.swift
@@ -1,16 +1,17 @@
 import NetworkExtension
 import SwiftUI
 
+@available(macOS 13.0, *)
 @main
 @MainActor
 struct BurrowApp: App {
     // To connect to the App Delegate
-    @NSApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    @NSApplicationDelegateAdaptor(AppDelegate.self)
+    var delegate
     var body: some Scene {
         WindowGroup {
-            OnboardingView()
-        }.windowStyle(.hiddenTitleBar)
-            
+            OnboardingView().frame(width: 1000, height: 600.0).scaledToFill().fixedSize()
+        }.windowStyle(.hiddenTitleBar).windowResizability(.contentSize)
     }
 }
 
@@ -20,10 +21,28 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         proto.serverAddress = "hackclub.com"
         manager.localizedDescription = "Burrow"
     }
-
+    // Verifies app status
+    func isFirstTime() -> Bool {
+        let launchedBefore = UserDefaults.standard.bool(forKey: "launchedBefore")
+        if launchedBefore {
+            print("Not first launch.")
+        } else {
+            print("First launch, setting UserDefault.")
+            setVisited()
+        }
+        return !launchedBefore
+    }
+    
     var statusItem: NSStatusItem?
     var popOver = NSPopover()
     func applicationDidFinishLaunching(_ notification: Notification) {
+        //Closes main window if it is not the first time
+        if !isFirstTime(){
+            if let window = NSApplication.shared.windows.first {
+                window.close()
+            }
+        }
+        
         let menuView = MenuView(tunnel: AppDelegate.tunnel)
         // Creating apopOver
         popOver.behavior = .transient
