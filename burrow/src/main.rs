12f7717c23ea80f768743226d22bdf167c5a6bc1
Sam Poder <sam.r.poder@gmail.com>
2023-06-06 15:05:42 +0000

    Begin CLI Work
    Uses the Parser module + it sets up a help & version.

--- burrow/src/main.rs
+++ burrow/src/main.rs
@@ -1,5 +1,14 @@
+use clap::Parser;
 use tokio::io::Result;
 use tun::TunInterface;
+
+#[derive(Parser)]
+#[command(name = "Burrow")]
+#[command(author = "Hack Club <team@hackclub.com>")]
+#[command(version = "0.1")]
+#[command(about = "Burrow is a tool for burrowing through firewalls, built by teenagers at Hack Club.", long_about = None)]
+
+struct Cli {}
 
 async fn try_main() -> Result<()> {
     let iface = TunInterface::new()?;
@@ -10,5 +19,6 @@ async fn try_main() -> Result<()> {
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
+    let cli = Cli::parse();
     try_main().await.unwrap();
 }
