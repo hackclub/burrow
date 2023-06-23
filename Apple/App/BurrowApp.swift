32e4e9d1d708a5bbec08f57384f611e31af0ad2b
Conrad Kramer <conrad@conradkramer.com>
2023-06-23 18:22:26 -0400

    Refactored MenuView into MenuItemToggleView
    This change fixes some subtle state bugs in the switch handling by
    making isOn a direct function of the Tunnel.

--- Apple/App/BurrowApp.swift
+++ Apple/App/BurrowApp.swift
@@ -1,12 +1,17 @@
-import NetworkExtension
 import SwiftUI
 
 @main
 @MainActor
 struct BurrowApp: App {
-    //To connect to the App Delegate
-    @NSApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    static let tunnel = Tunnel { manager, proto in
+        proto.serverAddress = "hackclub.com"
+        manager.localizedDescription = "Burrow"
+    }
 
+    #if os(macOS)
+    @NSApplicationDelegateAdaptor(AppDelegate.self)
+    var delegate
+    #endif
 
     var body: some Scene {
         WindowGroup {
@@ -14,40 +19,3 @@ struct BurrowApp: App {
         }
     }
 }
-
-
-@MainActor
-class AppDelegate: NSObject, NSApplicationDelegate {
-
-    static let tunnel = Tunnel { manager, proto in
-        proto.serverAddress = "hackclub.com"
-        manager.localizedDescription = "Burrow"
-    }
-
-    var statusItem: NSStatusItem?
-    var popOver = NSPopover()
-    func applicationDidFinishLaunching(_ notification: Notification) {
-        let menuView = MenuView(tunnel: AppDelegate.tunnel)
-        // Creating apopOver
-        popOver.behavior = .transient
-        popOver.animates = true
-        popOver.contentViewController = NSViewController()
-        popOver.contentViewController?.view = NSHostingView(rootView: menuView)
-        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-        // Safe Check if status Button is Available or not...
-        if let menuButton = statusItem?.button {
-            let icon = "network.badge.shield.half.filled"
-            menuButton.image = NSImage(systemSymbolName: icon, accessibilityDescription: nil)
-            menuButton.action = #selector(menuButtonToggle)
-        }
-    }
-    @objc func menuButtonToggle() {
-
-
-        if let menuButton = statusItem?.button {
-
-            self.popOver.show(relativeTo: menuButton.bounds, of: menuButton, preferredEdge: NSRectEdge.minY)
-        }
-    }
-}
-
