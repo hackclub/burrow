0bb4eb0b517a479ea027306c8129b2d73552e422
SerenityUX <thomas@serenidad.app>
2023-06-23 20:19:10 -0400

    Added initial menu bar to macOS

--- Apple/App/TunnelView.swift
+++ Apple/App/TunnelView.swift
@@ -1,35 +1,36 @@
 import SwiftUI
 
 struct TunnelView: View {
-    @ObservedObject var tunnel: Tunnel
+//    @ObservedObject var tunnel: Tunnel
 
     var body: some View {
-        VStack {
-            Text(verbatim: tunnel.status.description)
-            switch tunnel.status {
-            case .connected:
-                Button("Disconnect", action: stop)
-            case .permissionRequired:
-                Button("Allow", action: configure)
-            case .disconnected:
-                Button("Start", action: start)
-            default:
-                EmptyView()
-            }
-        }
-        .task { await tunnel.update() }
-        .padding()
+        EmptyView()
+//        VStack {
+//            Text(verbatim: tunnel.status.description)
+//            switch tunnel.status {
+//            case .connected:
+//                Button("Disconnect", action: stop)
+//            case .permissionRequired:
+//                Button("Allow", action: configure)
+//            case .disconnected:
+//                Button("Start", action: start)
+//            default:
+//                EmptyView()
+//            }
+//        }
+//        .task { await tunnel.update() }
+//        .padding()
     }
 
-    private func start() {
-        try? tunnel.start()
-    }
-
-    private func stop() {
-        tunnel.stop()
-    }
-
-    private func configure() {
-        Task { try await tunnel.configure() }
-    }
+//    private func start() {
+//        try? tunnel.start()
+//    }
+//
+//    private func stop() {
+//        tunnel.stop()
+//    }
+//
+//    private func configure() {
+//        Task { try await tunnel.configure() }
+//    }
 }
