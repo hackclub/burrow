3ef13b09a3613882aa9041b7e29cba0b7e9ba31b
Jett Chen <jettchen12345@gmail.com>
2023-08-06 11:51:50 +0800

    Add C Swift Bindings
    This adds C Swift bindings for burrow via compiling burrow
    with a matching header file.

--- Apple/NetworkExtension/PacketTunnelProvider.swift
+++ Apple/NetworkExtension/PacketTunnelProvider.swift
@@ -1,7 +1,22 @@
+import libburrow
 import NetworkExtension
+import OSLog
 
 class PacketTunnelProvider: NEPacketTunnelProvider {
+    let logger = Logger(subsystem: "com.hackclub.burrow", category: "General")
     override func startTunnel(options: [String: NSObject]?, completionHandler: @escaping (Error?) -> Void) {
+        let fild = libburrow.retrieve()
+        if fild == -1 {
+            // Not sure if this is the right way to return an error
+            logger.error("Failed to retrieve file descriptor for burrow.")
+            let err = NSError(
+                domain: "com.hackclub.burrow",
+                code: 1_010,
+                userInfo: [NSLocalizedDescriptionKey: "Failed to find TunInterface"]
+            )
+            completionHandler(err)
+        }
+        logger.info("fd: \(fild)")
         completionHandler(nil)
     }
 
--- burrow/src/lib.rs
+++ burrow/src/lib.rs
@@ -1,1 +1,32 @@
 pub mod ensureroot;
+
+#[cfg(any(target_os = "linux", target_vendor = "apple"))]
+use std::{
+    mem,
+    os::fd::{AsRawFd, FromRawFd},
+};
+
+use tun::TunInterface;
+
+// TODO Separate start and retrieve functions
+
+#[cfg(any(target_os = "linux", target_vendor = "apple"))]
+#[no_mangle]
+pub extern "C" fn retrieve() -> i32 {
+    let iface2 = (1..100)
+        .filter_map(|i| {
+            let iface = unsafe { TunInterface::from_raw_fd(i) };
+            match iface.name() {
+                Ok(_name) => Some(iface),
+                Err(_) => {
+                    mem::forget(iface);
+                    None
+                }
+            }
+        })
+        .next();
+    match iface2 {
+        Some(iface) => iface.as_raw_fd(),
+        None => -1,
+    }
+}
