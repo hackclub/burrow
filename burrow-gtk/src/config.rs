6d4fbcd078bf915ca4b3759da97cb41a89a50157
dav <usr21p1@gmail.com>
2023-11-04 18:00:12 -0700

    Small Fixes for Flatpak Building
    Be sure to install gnome 45 not 44
    Build: `flatpak-builder --keep-build-dirs --user --disable-rofiles-fuse flatpak_app com.hackclub.burrow.json`

--- burrow-gtk/src/config.rs
+++ burrow-gtk/src/config.rs
@@ -1,4 +1,4 @@
 pub static VERSION: &str = "0.1.0";
 pub static GETTEXT_PACKAGE: &str = "burrow-gtk";
-pub static LOCALEDIR: &str = "/app/share/locale";
-pub static PKGDATADIR: &str = "/app/share/burrow-gtk";
+pub static LOCALEDIR: &str = "/usr/local/share/locale";
+pub static PKGDATADIR: &str = "/usr/local/share/burrow-gtk";
