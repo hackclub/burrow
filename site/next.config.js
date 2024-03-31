ec8cc533abf7502cb09ea6f8033124786afe5fb7
Conrad Kramer <conrad@conradkramer.com>
2024-03-30 17:17:52 -0700

    Add apple-app-site-association file

--- site/next.config.js
+++ site/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  headers() {
+    return [
+      {
+        source: "/.well-known/apple-app-site-association",
+        headers: [{ key: "Content-Type", value: "application/json" }],
+      }
+    ];
+  }
 };
 
 module.exports = nextConfig;
